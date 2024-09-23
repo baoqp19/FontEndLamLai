06e7f8905a500a7d2e67245da949ea1f5b67c657
= <=>
2024-09-23 21:01:10 +0700

    Create Login Register Sass configts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv, type PluginOption } from 'vite'
+// PluginOption: Kiểu cho các plugin trong Vite.
 import react from '@vitejs/plugin-react-swc'
 import path from 'path';
+import { visualizer } from "rollup-plugin-visualizer";
+
+// rollup-plugin-visualizer: Plugin để trực quan hóa kích thước bundle.
 
 // https://vitejs.dev/config/
 // https://v2.vitejs.dev/config/#environment-variables
@@ -9,7 +13,8 @@ export default defineConfig(({ command, mode }) => {
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    plugins: [react()],
+    // visualizer(): Để tạo biểu đồ hiển thị kích thước bundle.
+    plugins: [react(), visualizer() as PluginOption],  
     server: {
       port: parseInt(env.PORT)
     },
@@ -17,6 +22,7 @@ export default defineConfig(({ command, mode }) => {
       alias: {
         "@": path.resolve(__dirname, "./src/"),
         components: `${path.resolve(__dirname, "./src/components/")}`,
+        styles: `${path.resolve(__dirname, "./src/styles/")}`,
       },
     },
   }
